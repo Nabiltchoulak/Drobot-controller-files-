b18bd5b3e49efbab66455077723937f256cd1f77
Nabiltchoulak <40767112+Nabiltchoulak@users.noreply.github.com>
2019-03-10 01:08:51 +0100

    Add files via upload

---
+++ route_node.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from sensor_msgs.msg import Image
+
+rospy.init_node('router', anonymous=True)
+pub = rospy.Publisher('/cam_front/left/image_rect_color', Image,queue_size=10)
+pub2= rospy.Publisher('/cam_front/right/image_rect_color', Image,queue_size=10)
+def ORSinterface():
+   
+   
+   
+   rate = rospy.Rate(60) # 10hz
+   while not rospy.is_shutdown():
+      #hello_str = "hello world %s" % rospy.get_time()
+      #rospy.loginfo(hello_str)
+      rospy.Subscriber("/uav/camera/left/image_rect_color", Image, callback)
+      rospy.Subscriber("/uav/camera/left/image_rect_color", Image, callback2)
+      #rate.sleep()
+
+
+def callback2(data):
+   pub2.publish(data)
+def callback(data):
+   pub.publish(data)
+
+   #rospy.loginfo("I heard %s",data.data)
+   
+
+if __name__ == '__main__':
+   try:
+      ORSinterface()
+
+   except rospy.ROSInterruptException:
+      pass
