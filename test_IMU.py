cea065d195ee001d3df27a16b107614694ed0fd4
Nabiltchoulak <40767112+Nabiltchoulak@users.noreply.github.com>
2019-03-21 16:04:53 +0100

    Update test_IMU.py

--- test_IMU.py
+++ test_IMU.py
@@ -13,6 +13,8 @@ from tf2_msgs.msg import TFMessage
 import tf_conversions
 import geometry_msgs.msg
 
+
+
 def pqr_to_phidot_thetadot_psidot(p, q, r, theta, phi):
     phi_dot = p + q * sin(phi) * tan(theta) + r * cos(phi) * tan(theta)
     theta_dot = q * cos(phi) - r * sin(phi)
@@ -65,11 +67,11 @@ def get_angle_gyro(roll, pitch, yaw, gyro_x, gyro_y, gyro_z, dt):
     pitch_dot = cos(roll) * gyro_y - sin(roll) * gyro_z
     yaw_dot = sin(roll) / cos(pitch) * gyro_y + cos(roll) / cos(pitch) * gyro_z
 
-    #roll_dot, pitch_dot, yaw_dot = pqr_to_phidot_thetadot_psidot(roll_dot, pitch_dot,
+    # roll_dot, pitch_dot, yaw_dot = pqr_to_phidot_thetadot_psidot(roll_dot, pitch_dot,
     #                                                             yaw_dot, pitch, roll)
-    new_roll = roll + roll_dot * dt[0]
-    new_pitch = pitch + pitch_dot * dt[0]
-    new_yaw = yaw + yaw_dot * dt[0]
+    new_roll = roll + roll_dot * dt
+    new_pitch = pitch + pitch_dot * dt
+    new_yaw = yaw + yaw_dot * dt
     return new_roll, new_pitch, new_yaw
 
 
@@ -78,30 +80,29 @@ def get_angle_accelerometer(accelerometer_x, accelerometer_y, accelerometer_z, t
     g = 9.80665
     pi = 3.141592
     if type == 'degre':
-        roll_accelerometer = atan2(accelerometer_y,
-                                        sqrt((accelerometer_z + g) ^ 2 + accelerometer_x ^ 2)) * 180 / pi
+        roll_accelerometer = atan2(accelerometer_y, accelerometer_x) * 180 / pi
         pitch_accelerometer = atan2(accelerometer_x,
-                                         sqrt((accelerometer_z + g) ^ 2 + accelerometer_y ^ 2)) * 180 / pi
-        yaw_accelerometer = atan2(accelerometer_x, accelerometer_y) * 180 / pi
+                                         sqrt(accelerometer_z ^ 2 + accelerometer_y ^ 2)) * 180 / pi
+        yaw_accelerometer = 0
     if type == 'radians':
-        roll_accelerometer = atan2(accelerometer_y,
-                                        sqrt((accelerometer_z + g) ** 2 + accelerometer_x ** 2))
+        roll_accelerometer = atan2(accelerometer_y, accelerometer_x)
         pitch_accelerometer = atan2(accelerometer_x,
-                                         sqrt((accelerometer_z + g) ** 2 + accelerometer_y ** 2))
-        yaw_accelerometer = atan2(accelerometer_x, accelerometer_y)
-
+                                         sqrt(accelerometer_z ** 2 + accelerometer_y ** 2))
+        yaw_accelerometer = 0
     else:
         print('type error in get_angle_acc')
     return roll_accelerometer, pitch_accelerometer, yaw_accelerometer
 
 
-def callback(data, dt):
-    
-    alpha = 0.1  # to choose between 0 and 1 preferably [0.02, 0.1] hkaya hna pk
-    pitch, roll, yaw = toEulerAngle(data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w)
-    roll_acc, pitch_acc, yaw_acc = get_angle_accelerometer(data.linear_acceleration.x, data.linear_acceleration.y,data.linear_acceleration.z)
 
-    roll_gyro, pitch_gyro, yaw_gyro = get_angle_gyro(roll, pitch, yaw, data.angular_velocity.x, data.angular_velocity.y,data.angular_velocity.z, dt)
+def callback(data, dt):
+    alpha = 0.02  # to choose between 0 and 1 preferably [0.02, 0.1] hkaya hna pk
+    pitch, roll, yaw = toEulerAngle(data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w)
+    roll_acc, pitch_acc, yaw_acc = get_angle_accelerometer(data.linear_acceleration.x, data.linear_acceleration.y,
+                                                           data.linear_acceleration.z)
+
+    roll_gyro, pitch_gyro, yaw_gyro = get_angle_gyro(roll, pitch, yaw, data.angular_velocity.x, data.angular_velocity.y,
+                                                     data.angular_velocity.z, dt[0])
     roll_new = (1 - alpha) * roll_gyro + alpha * roll_acc
     pitch_new = (1 - alpha) * pitch_gyro + alpha * pitch_gyro
     # For the yaw angle we will use only the gyro so (a = 1)
@@ -120,29 +121,24 @@ def callback(data, dt):
     sender.transform.rotation.y = q2
     sender.transform.rotation.z = q3
     sender.transform.rotation.w = q4
-    
-    
 
-
-    
     """t.transform.translation.x = data.x
     t.transform.translation.y = data.y
     t.transform.translation.z = 0.0"""
-    
+
     sender.transform.rotation.x = q1
     sender.transform.rotation.y = q2
     sender.transform.rotation.z = q3
     sender.transform.rotation.w = q4
 
-    br.sendTransform(sender)##### new sender more convenient 
-    print(sender)
+    br.sendTransform(sender)  ##### new sender more convenient
+    #print(sender)
 
-    #pub.publish(sender)
+    # pub.publish(sender)
 
 
 if __name__ == '__main__':
     rospy.init_node('data_fusion', anonymous=True)
-    
 
     rate = rospy.Rate(1000)
     dt = 0.001
