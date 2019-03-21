35e32e3a0804bbcf3c7e170cdc527a8a4a99b2b8
Nabiltchoulak <40767112+Nabiltchoulak@users.noreply.github.com>
2019-03-21 15:04:08 +0100

    New tf sender structure

--- test_IMU.py
+++ test_IMU.py
@@ -9,6 +9,9 @@ import numpy as np
 import tf2_ros
 from sensor_msgs.msg import Imu
 from tf2_msgs.msg import TFMessage
+# Because of transformations
+import tf_conversions
+import geometry_msgs.msg
 
 def pqr_to_phidot_thetadot_psidot(p, q, r, theta, phi):
     phi_dot = p + q * sin(phi) * tan(theta) + r * cos(phi) * tan(theta)
@@ -75,15 +78,17 @@ def get_angle_accelerometer(accelerometer_x, accelerometer_y, accelerometer_z, t
     g = 9.80665
     pi = 3.141592
     if type == 'degre':
-        roll_accelerometer = atan2(accelerometer_y, accelerometer_x) * 180 / pi
+        roll_accelerometer = atan2(accelerometer_y,
+                                        sqrt((accelerometer_z + g) ^ 2 + accelerometer_x ^ 2)) * 180 / pi
         pitch_accelerometer = atan2(accelerometer_x,
                                          sqrt((accelerometer_z + g) ^ 2 + accelerometer_y ^ 2)) * 180 / pi
-        yaw_accelerometer = 0
+        yaw_accelerometer = atan2(accelerometer_x, accelerometer_y) * 180 / pi
     if type == 'radians':
-        roll_accelerometer = atan2(accelerometer_y, accelerometer_x)
+        roll_accelerometer = atan2(accelerometer_y,
+                                        sqrt((accelerometer_z + g) ** 2 + accelerometer_x ** 2))
         pitch_accelerometer = atan2(accelerometer_x,
                                          sqrt((accelerometer_z + g) ** 2 + accelerometer_y ** 2))
-        yaw_accelerometer = 0
+        yaw_accelerometer = atan2(accelerometer_x, accelerometer_y)
 
     else:
         print('type error in get_angle_acc')
@@ -91,13 +96,12 @@ def get_angle_accelerometer(accelerometer_x, accelerometer_y, accelerometer_z, t
 
 
 def callback(data, dt):
+    
     alpha = 0.1  # to choose between 0 and 1 preferably [0.02, 0.1] hkaya hna pk
     pitch, roll, yaw = toEulerAngle(data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w)
-    roll_acc, pitch_acc, yaw_acc = get_angle_accelerometer(data.linear_acceleration.x, data.linear_acceleration.y,
-                                                           data.linear_acceleration.z)
+    roll_acc, pitch_acc, yaw_acc = get_angle_accelerometer(data.linear_acceleration.x, data.linear_acceleration.y,data.linear_acceleration.z)
 
-    roll_gyro, pitch_gyro, yaw_gyro = get_angle_gyro(roll, pitch, yaw, data.angular_velocity.x, data.angular_velocity.y,
-                                                     data.angular_velocity.z, dt)
+    roll_gyro, pitch_gyro, yaw_gyro = get_angle_gyro(roll, pitch, yaw, data.angular_velocity.x, data.angular_velocity.y,data.angular_velocity.z, dt)
     roll_new = (1 - alpha) * roll_gyro + alpha * roll_acc
     pitch_new = (1 - alpha) * pitch_gyro + alpha * pitch_gyro
     # For the yaw angle we will use only the gyro so (a = 1)
@@ -105,23 +109,40 @@ def callback(data, dt):
 
     print('new', roll_new, pitch_new, yaw_new)
     print('old', roll, pitch, yaw)
-    sender = TFMessage()
-    sender.header.stamp = rospy.Time.now()
-    sender.header.frame_id = "uav/imu"
+    sender = geometry_msgs.msg.TransformStamped()
+    br = tf2_ros.TransformBroadcaster()
 
+    sender.header.stamp = rospy.Time.now()
+    sender.header.frame_id = "world"
+    sender.child_frame_id = "fused_imu"
     q1, q2, q3, q4 = euler_to_quaternions(roll_new, pitch_new, yaw_new)
     sender.transform.rotation.x = q1
     sender.transform.rotation.y = q2
     sender.transform.rotation.z = q3
     sender.transform.rotation.w = q4
+    
+    
+
+
+    
+    """t.transform.translation.x = data.x
+    t.transform.translation.y = data.y
+    t.transform.translation.z = 0.0"""
+    
+    sender.transform.rotation.x = q1
+    sender.transform.rotation.y = q2
+    sender.transform.rotation.z = q3
+    sender.transform.rotation.w = q4
+
+    br.sendTransform(sender)##### new sender more convenient 
+    print(sender)
 
     #pub.publish(sender)
 
 
 if __name__ == '__main__':
     rospy.init_node('data_fusion', anonymous=True)
-    pub = rospy.Publisher('fused_data', TFMessage, queue_size=5)
-
+    
 
     rate = rospy.Rate(1000)
     dt = 0.001
